723c31055a2ebe64981e7e6625c2ff9d89effa50
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2015-09-22 15:37:12 -0700

    linked budget, added db location, refactored alot of the code, added submit button instead of auto load from input

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('exploretipApp')
-    .controller('MainCtrl', function($scope, $http, socket, uiGmapGoogleMapApi) {
+    .controller('MainCtrl', function($scope, $http, socket, uiGmapGoogleMapApi, $filter) {
         $scope.awesomeThings = [];
 
         $http.get('/api/things').then(function(response) {
@@ -69,7 +69,7 @@ angular.module('exploretipApp')
             showweeks: false,
             mode: "month"
         };
-
+        
         $scope.showSelected = function(input) {
             var object = [];
             for (var o in input) {
@@ -80,14 +80,41 @@ angular.module('exploretipApp')
             return object;
         };
 
-        $scope.getCurrentValue = function(specificLocation) {
+
+		// submit button && expedia api call
+        $scope.getCurrentValue = function() {
             //console.log("inside getCurrentValue");
             //console.log("inside getCurrentValue2");
             $http.post('/api/things', {
-                name: "$"+$scope.budget.defaultValue+", "+$scope.showSelected($scope.where.defaultValue)+", "+$scope.when.defaultValue+", "+specificLocation
+                name: "$"+$scope.budget.defaultValue+", "+$scope.showSelected($scope.where.defaultValue)+", "+$scope.when.defaultValue+", "+$scope.specificLocation
             });
             $scope.newThing = '';
+            
+            $scope.expediaReturn($scope.specificLocation); 
         };
+        
+        
+        // watch functions for budget && calendar && where
+		$scope.$watch('budget.defaultValue', function(){
+			console.log($scope.budget.defaultValue);
+		});
+		
+		$scope.$watch('when.defaultValue', function(){
+			//console.log($scope.when.defaultValue);
+			console.log($filter('date')($scope.when.defaultValue, 'MM/dd/yyyy'));
+		});
+		
+		$scope.showSelected = function(input) {
+            //console.log(input)
+            var object = [];
+            for (var o in input) {
+                if (input[o]) {
+                    object.push(o);
+                }
+            }
+            return object;
+        };
+
 
         // End accordion functions ======================================================
 
@@ -103,6 +130,7 @@ angular.module('exploretipApp')
             	labelIndex = 0,
 				infowindow = new google.maps.InfoWindow(),
 				marker,
+				markersArray = [],
 				locations = [],
 				i;
 				
@@ -121,30 +149,30 @@ angular.module('exploretipApp')
             
             
             // set markers from results on map
-			var markersDisplay = function(lat, lng) {
+			$scope.markersDisplay = function(lat, lng) {
 				marker = new google.maps.Marker({
-                                position: new google.maps.LatLng(lat, lng),
-                                map: map,
-                                label: labels[labelIndex++ % labels.length],
-                                animation: google.maps.Animation.DROP
-                            });
+                    position: {lat, lng},
+                    map: map,
+                    label: labels[labelIndex++ % labels.length],
+                    animation: google.maps.Animation.DROP
+                });
+                markersArray.push(marker);
 			}
 			
-			
-			// remove markers from map
-			var markersArray = [];
-			
-			function clearOverlays() {
-				for (var i = 0; i < markersArray.length; i++ ) {
-					markersArray[i].setMap(null);
-				}
-				markersArray.length = 0;
-				$('.results').empty(); 
+			// google recommended way to remove markers, 3 functions below
+			$scope.setMapOnAll = function(map) {
+			  for (var i = 0; i < markersArray.length; i++) {
+			    markersArray[i].setMap(map);
+			  }
 			}
-			
-			$("#removeIt").on( "click", function() {
-				clearOverlays();
-			});
+			$scope.clearMarkers = function() {
+			  $scope.setMapOnAll(null);
+			}
+			$scope.deleteMarkers = function() {
+			  $scope.clearMarkers();
+			  markersArray = [];
+			  $('.results').empty(); 
+			}
 			
 
 			// init autocomplete on input search
@@ -153,8 +181,24 @@ angular.module('exploretipApp')
             var autocomplete = new google.maps.places.Autocomplete(input, options);
 
 
-			// expedia return ------------------------------------------------------------
-			function expediaReturn(specificLocation) {
+			// move map to city and call expedia api
+			$scope.panMap = function(specificLocation) {
+				var place = autocomplete.getPlace();
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                }
+			}
+			
+			
+			// expedia return ------------------------------------------
+			$scope.expediaReturn = function(specificLocation) {
+								
+				//var startDate = $filter('date')($scope.when.defaultValue, 'MM/dd/yyyy');
+				//var endDate = '';
+				//console.log(startDate);   
+				//console.log(endDate);
 				
 				// expedia required call parameters
                 var place = autocomplete.getPlace(),
@@ -165,12 +209,12 @@ angular.module('exploretipApp')
                     curencyCode = 'USD',
                     adults = '2',
                     destinationString = specificLocation,
-                    arrivalDate = '11/17/2015',
+                    arrivalDate = '11/18/2015',
                     departureDate = '11/19/2015',
                     room = '2',
                     sort = 'PRICE', 
                     maxResults = '20';
-                    
+                                    
                 $.ajax({
                     type: 'GET',
                     url: 'http://api.ean.com/ean-services/rs/hotel/v3/list?locale='+locale+'&destinationString='+destinationString+'&apiKey='+apiKey+'&minorRev='+minorRev+'&departureDate='+departureDate+'&room='+room+'&arrivalDate='+arrivalDate+'&curencyCode='+curencyCode+'&cid='+cid+'&numberOfResults='+maxResults+'&Room.numberOfAdults='+adults+'&sort='+sort+'',
@@ -179,7 +223,7 @@ angular.module('exploretipApp')
                     dataType: 'jsonp',
 
                     success: function(data) {
-												
+	                    												
                         $.each(data.HotelListResponse.HotelList.HotelSummary, function(k, v) {
 	                        
 	                        var averageRate = v.RoomRateDetailsList.RoomRateDetails.RateInfos.RateInfo.ChargeableRateInfo["@averageRate"];
@@ -212,12 +256,11 @@ angular.module('exploretipApp')
 	                                                        
 	                                                        
 	                            // set new makers on the map and side nav
-	                            markersDisplay(locations[i].lat, locations[i].lng);
-	                            markersArray.push(marker); // push into array to later remove markers
+	                            $scope.markersDisplay(locations[i].lat, locations[i].lng);
 								$('.results').append(hotelResults);
 								
 								
-	                            // on marker click show hotel info -- not needed for now
+	                            // on marker click show hotel info
 	                            google.maps.event.addListener(marker, 'click', (function(marker, i) {
 	                                return function() {
 	                                    infowindow.setContent("<img src=\"http://images.travelnow.com/"+locations[i].hotelThumb+"\" alt=\""+locations[i].hotelName+"\" class=\"hotelImg\"> <span class=\"hotelTitle\">"+locations[i].hotelName+"</span> <br>Average Nightly: $"+locations[i].hotelRateAverage+"<br> Total: $"+locations[i].hotelRateTotal+"<br><img src=\""+locations[i].hotelRatingImg+"\" class=\"tripAdvisorRating\"><br><button type=\"button\" class=\"btn btn-default\"><a href=\""+locations[i].hotelLink+"\" target=\"_blank\">Seek Deer <i class=\"fa fa-hand-peace-o\"></i></a></button><hr>"); 	
@@ -230,9 +273,7 @@ angular.module('exploretipApp')
 							} else {
 							  
 							}
-	                        
-	                                                   
-                                                        
+                                   
                         }
                         
                     },
@@ -248,34 +289,21 @@ angular.module('exploretipApp')
                 } else {
                     map.setCenter(place.geometry.location);
                 }
-			} // end expedia return ------------------------------------------------------------
-			
+			} 
 
+            // end expedia return --------------------------------------
+            
+			
             // event handler for autocomplete change
             google.maps.event.addListener(autocomplete, 'place_changed', function() {
-	            
-	            var specificLocation = autocomplete.getPlace().formatted_address;
-	            expediaReturn(specificLocation);
-	            $scope.getCurrentValue(specificLocation);
-                
+	            $scope.specificLocation = autocomplete.getPlace().formatted_address;
+	            $scope.panMap();
             }); 
 
 
-        }); // end google maps sdk ======================================================
+        }); 
+        // end google maps sdk ======================================================
 
-
-
-
-        $scope.showSelected = function(input) {
-            //console.log(input)
-            var object = [];
-            for (var o in input) {
-                if (input[o]) {
-                    object.push(o);
-                }
-            }
-            return object;
-        };
 
 
     });
